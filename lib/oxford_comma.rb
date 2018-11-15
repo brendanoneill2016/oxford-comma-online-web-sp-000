19dc6905a83cd582f4b86fa59f1c9d3369c664be
Brendan O'Neill <oneill.brendan88@gmail.com>
2018-11-15 08:57:30 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,11 +2,9 @@ def oxford_comma(array)
   
  if array.count == 1 
    array.join
- end
-   
- if array.count == 2 
-   array.insert(-2,'and')
+ elsif array.count == 2
+ array.insert(-2,'and')
    array.join(" ")
-end
+  end
 
 end
